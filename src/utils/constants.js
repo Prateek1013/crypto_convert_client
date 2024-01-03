603f1a8411654092595505fdb999174efb3ed884
Prateek1013 <prateek9771196112@gmail.com>
2024-01-04 01:37:07 +0530

    added NODE_ENV check

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
+const URL=process.env.NODE_ENV=== "production"? 'https://crypto-convert-server.onrender.com' : 'http://localhost:4000';
 export const BACKEND_API={
-    CRYPTO_URL:'http://localhost:4000/cryptos',
-    CURRENCIES_URL:'http://localhost:4000/currencies'
+    CRYPTO_URL:`${URL}/cryptos`,
+    CURRENCIES_URL:`${URL}/currencies`
 }
 
 
