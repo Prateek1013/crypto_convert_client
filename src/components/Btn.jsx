c486f4a49b74107d2af93f6ff6bc2946760f3e94
Prateek1013 <prateek9771196112@gmail.com>
2024-01-04 12:46:36 +0530

    chaned convert backend api

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -2,6 +2,7 @@ import { Button } from "antd";
 import { useDispatch,useSelector} from 'react-redux'
 import { showLoader } from "../redux-slice/loaderSlice";
 import { setShowAmount } from "../redux-slice/resultSlice";
+import { URL } from "../utils/constants";
 const Btn = () => {
   const {crypto,currency,amount}=useSelector(state => state.selectedVariables);
   const dispatch=useDispatch();
@@ -10,7 +11,7 @@ const Btn = () => {
           if(!crypto) { alert("please select crypto from dropdown"); return; }
           if(amount===0) { alert("please enter amount"); return; }
           dispatch(showLoader(true));
-          fetch(`http://localhost:4000/convert?crypto=${crypto}&currency=${currency}`).then(res=>res.json())
+          fetch(`${URL}/convert?crypto=${crypto}&currency=${currency}`).then(res=>res.json())
           .then(jsondata=>{
             dispatch(setShowAmount(amount*jsondata[crypto][currency]));
             dispatch(showLoader(false));
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,6 @@
-const URL=process.env.NODE_ENV=== "production"? 'https://crypto-convert-server.onrender.com' : 'http://localhost:4000';
+export const URL=process.env.NODE_ENV=== "production"? 'https://crypto-convert-server.onrender.com' : 'http://localhost:4000';
 export const BACKEND_API={
     CRYPTO_URL:`${URL}/cryptos`,
     CURRENCIES_URL:`${URL}/currencies`
 }
-
 
